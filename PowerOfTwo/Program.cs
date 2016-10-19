2e01addbf9884a27177611d604ea96d6a7b3547c
Adrian <adymartianul@gmail.com>
2016-10-19 13:42:44 +0300

    .

--- PowerOfTwo/Program.cs
+++ PowerOfTwo/Program.cs
@@ -10,6 +10,26 @@ namespace PowerOfTwo
     {
         static void Main(string[] args)
         {
+            int number = 15;
+
+            while (number > 0)
+            {
+                int temp = HighestPowerOfTwo(number);
+                Console.Write("{0} + ", temp);
+                number = number - temp;
+            }
+            Console.WriteLine();
+        }
+
+        static int HighestPowerOfTwo(int number)
+        {
+            int result = 1;
+            while (number > 1)
+            {
+                number = number / 2;
+                result = result * 2;
+            }
+            return result;
         }
     }
 }
